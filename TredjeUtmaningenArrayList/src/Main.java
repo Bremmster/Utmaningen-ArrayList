43860ae8d9fb2f1ac48a66f5942868065b281205
Bremmster <107461637+Bremmster@users.noreply.github.com>
2023-02-20 19:04:10 +0100

    add java files

---
+++ TredjeUtmaningenArrayList/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        /*Du ska skapa en namnlista med CRUD funktionalitet
+        Man saka kunna skapa namn, lista alla namn, ändra namn och radera namn
+        Om man försöker radera eller ändra ett namn som inte finns ska programmet säga till
+        om att namnet inte finns.
+
+        Vid inlämning, skicka in kodfilerna (*.java) bara */
+
+        /*
+        +---------------------------------------+
+        | Den coola namnlistan                  |
+        +---------------------------------------+
+        | Listan innehåller 0 namn              |
+        | 1) Mata in ett namn                   |
+        | 2) Radera ett namn                    |
+        | 3) Ändra ett namn                     |
+        | 4) Lista alla namn                    |
+        +---------------------------------------+
+         */
+
+
+    }
+}
