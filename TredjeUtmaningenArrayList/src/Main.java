5fd5b2a1cab8c23d325f671a0b61d4ed4b5d69fd
Bremmster <107461637+Bremmster@users.noreply.github.com>
2023-02-21 09:21:35 +0100

    new metods for C.R.L

--- TredjeUtmaningenArrayList/src/Main.java
+++ TredjeUtmaningenArrayList/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         /*Du ska skapa en namnlista med CRUD funktionalitet
@@ -6,17 +9,48 @@ public class Main {
         om att namnet inte finns.
 
         Vid inlämning, skicka in kodfilerna (*.java) bara */
+        ArrayList<String> names = new ArrayList<>();
 
-        int numberOfNames = 2; // numberOfNames ska bort och istället vara storleken på listan
-        printMenu(numberOfNames); // numberOfNames ska bort och istället vara storleken på listan
+        printMenu(names); // Klar
+        addName(names); // Klar
+        removeName(names);
+        changeName(names);
+        listNames(names); // Klar
     }
 
-    private static void printMenu(int numberOfNames) {
-        // numberOfNames ska bort och istället vara storleken på listan
+    private static void listNames(ArrayList<String> names) {
+        System.out.println("Listan innehåller följande namn:");
+        for (String name : names){
+            System.out.println(name);
+        }
+    }
+
+    private static void changeName(ArrayList<String> namn) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Sök och ersätt. namn: ");
+        String replace = scanner.nextLine(); // om sant ersätt
+        if (namn.contains(replace)){
+            System.out.print("Namnet finns, vad vill du ersätta med: ");
+            String newName = scanner.nextLine();
+            int pos = namn.indexOf(replace);
+            namn.set(pos, newName);
+        } else System.out.println("Namnet finns inte i listan.");
+    }
+    private static void removeName(ArrayList<String> namn) {
+        // namn.remove
+
+    }
+    private static void addName(ArrayList<String> namn) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Namn att lägga till: ");
+        namn.add(scanner.nextLine());
+    }
+
+    private static void printMenu(ArrayList<String> namn) {
         System.out.println("+---------------------------------------+");
         System.out.println("| Den Crudiga namnlistan                |");
         System.out.println("+---------------------------------------+");
-        System.out.println("| Listan innehåller " + numberOfNames + " namn              |");
+        System.out.println("| Listan innehåller " + namn.size() + " namn              |");
         System.out.println("| 1) Mata in ett namn                   |");
         System.out.println("| 2) Radera ett namn                    |");
         System.out.println("| 3) Ändra ett namn                     |");
